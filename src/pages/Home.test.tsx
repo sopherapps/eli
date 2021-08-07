ee76bd47f6dbfbc27787f5cf640bd0c319788164
Martin Ahindura <martinahindura@gmail.com>
2021-08-07 17:08:26 +0300

    Add snapshot test

--- src/pages/Home.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import Home from "./Home";
-
-test("renders the home screen correctly", () => {
-  const tree = renderer.create(<Home />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
