989be92642fba28e97f8d0c4b9c7dbee3a9fcfa5
Martin Ahindura <martinahindura@gmail.com>
2021-08-10 02:25:00 +0300

    Style the default fields of the form

--- src/data/models.ts
+++ src/data/models.ts
@@ -43,13 +43,13 @@ export interface Visualization {
 export interface VisualizationType {
   name: string;
   icon: string;
-  config: { [key: string]: VisualizationProp };
+  config: VisualizationProp[];
 }
 
 export interface VisualizationProp {
+  name: string;
   label: string;
   inputType: HTMLInputType;
-  orderPosition: number;
   options: { [key: string]: any };
   value?: any;
 }
@@ -62,7 +62,6 @@ export enum HTMLInputType {
   Email,
   Url,
   TextArea,
-  Radio,
   Checkbox,
   Range,
   Color,
@@ -71,7 +70,7 @@ export enum HTMLInputType {
   Time,
   Week,
   Select,
-  Button,
+  AddMoreButton,
 }
 
 /* Visualization Types */
@@ -79,525 +78,542 @@ export enum HTMLInputType {
 export const TableType: VisualizationType = {
   name: "table",
   icon: whiteTableIcon,
-  config: {
-    columnField: {
+  config: [
+    {
+      name: "columnField",
       label: "column field",
       inputType: HTMLInputType.Text,
-      orderPosition: 1,
       options: {},
     },
-    rowField: {
+    {
+      name: "rowField",
       label: "row field",
       inputType: HTMLInputType.Text,
-      orderPosition: 2,
       options: {},
     },
-    valueField: {
+    {
+      name: "valueField",
       label: "value field",
       inputType: HTMLInputType.Text,
-      orderPosition: 3,
       options: {},
     },
-  },
+  ],
 };
 
 export const ScatterChartType: VisualizationType = {
   name: "scatter-chart",
   icon: whiteScatterChartIcon,
-  config: {
-    label: {
+  config: [
+    {
+      name: "label",
       label: "label",
       inputType: HTMLInputType.Text,
-      orderPosition: 1,
       options: {},
     },
-    color: {
+    {
+      name: "color",
       label: "color",
       inputType: HTMLInputType.Color,
-      orderPosition: 2,
       options: {},
     },
-    xField: {
+    {
+      name: "xField",
       label: "X-axis field",
       inputType: HTMLInputType.Text,
-      orderPosition: 3,
       options: {},
     },
-    yField: {
+    {
+      name: "yField",
       label: "Y-axis field",
       inputType: HTMLInputType.Text,
-      orderPosition: 4,
       options: {},
     },
-  },
+  ],
 };
 
 export const BarChartType: VisualizationType = {
   name: "bar-chart",
   icon: whiteBarChartIcon,
-  config: {
-    label: {
+  config: [
+    {
+      name: "label",
       label: "label",
       inputType: HTMLInputType.Text,
-      orderPosition: 1,
       options: {},
     },
-    color: {
+    {
+      name: "color",
       label: "color",
       inputType: HTMLInputType.Color,
-      orderPosition: 2,
       options: {},
     },
-    xField: {
+    {
+      name: "xField",
       label: "x-axis field",
       inputType: HTMLInputType.Text,
-      orderPosition: 3,
       options: {},
     },
-    yField: {
+    {
+      name: "yField",
       label: "y-axis field",
       inputType: HTMLInputType.Text,
-      orderPosition: 4,
       options: {},
     },
-    chartStyle: {
+    {
+      name: "chartStyle",
       label: "chart style",
       inputType: HTMLInputType.Select,
-      orderPosition: 5,
       options: {
         options: ["stacked", "normal"],
         default: "normal",
       },
     },
-    orientation: {
+    {
+      name: "orientation",
       label: "orientation",
       inputType: HTMLInputType.Select,
-      orderPosition: 6,
       options: {
         options: ["horizontal", "vertical"],
         default: "horizontal",
       },
     },
-  },
+  ],
 };
 
 export const MultipleBarChartType: VisualizationType = {
   name: "multiple-bar-chart",
   icon: whiteMultipleBarChartIcon,
-  config: {
-    chartStyle: {
+  config: [
+    {
+      name: "chartStyle",
       label: "chart style",
       inputType: HTMLInputType.Select,
-      orderPosition: 1,
       options: {
         options: ["stacked", "normal"],
         default: "normal",
       },
     },
-    orientation: {
+    {
+      name: "orientation",
       label: "orientation",
       inputType: HTMLInputType.Select,
-      orderPosition: 2,
       options: {
         options: ["horizontal", "vertical"],
         default: "horizontal",
       },
     },
-    addDataset: {
+    {
+      name: "addDataset",
       label: "add dataset",
-      inputType: HTMLInputType.Button,
-      orderPosition: 3,
+      inputType: HTMLInputType.AddMoreButton,
       options: {
         datasets: [],
-        defaultDataset: {
-          name: {
+        defaultDataset: [
+          {
+            name: "name",
             label: "name",
             inputType: HTMLInputType.Text,
-            orderPosition: 1,
             options: {},
           },
-          label: {
+          {
+            name: "label",
             label: "label",
             inputType: HTMLInputType.Text,
-            orderPosition: 2,
             options: {},
           },
-          color: {
+          {
+            name: "color",
             label: "color",
             inputType: HTMLInputType.Color,
-            orderPosition: 3,
             options: {},
           },
-          xField: {
+          {
+            name: "xField",
             label: "x-axis field",
             inputType: HTMLInputType.Text,
-            orderPosition: 4,
             options: {},
           },
-          yField: {
+          {
+            name: "yField",
             label: "y-axis field",
             inputType: HTMLInputType.Text,
-            orderPosition: 5,
             options: {},
           },
-        },
+        ],
       },
     },
-  },
+  ],
 };
 
 export const LineChartType: VisualizationType = {
   name: "line-chart",
   icon: whiteLineChartIcon,
-  config: {
-    label: {
+  config: [
+    {
+      name: "label",
       label: "label",
       inputType: HTMLInputType.Text,
-      orderPosition: 1,
       options: {},
     },
-    color: {
+    {
+      name: "color",
       label: "color",
       inputType: HTMLInputType.Color,
-      orderPosition: 2,
       options: {},
     },
-    xField: {
+    {
+      name: "xField",
       label: "x-axis field",
       inputType: HTMLInputType.Text,
-      orderPosition: 3,
       options: {},
     },
-    yField: {
+    {
+      name: "yField",
       label: "y-axis field",
       inputType: HTMLInputType.Text,
-      orderPosition: 4,
       options: {},
     },
-    chartStyle: {
+    {
+      name: "chartStyle",
       label: "chart style",
       inputType: HTMLInputType.Select,
-      orderPosition: 5,
       options: {
         options: ["dotted", "normal"],
         default: "normal",
       },
     },
-    areaUnderTheLineColor: {
+    {
+      name: "areaUnderTheLineColor",
       label: "color of area under line",
       inputType: HTMLInputType.Color,
-      orderPosition: 6,
       options: {},
     },
-  },
+  ],
 };
 
 export const MultipleLineChartType: VisualizationType = {
   name: "multiple-line-chart",
   icon: whiteStackedLineChartIcon,
-  config: {
-    addDataset: {
+  config: [
+    {
+      name: "addDataset",
       label: "add dataset",
-      inputType: HTMLInputType.Button,
-      orderPosition: 3,
+      inputType: HTMLInputType.AddMoreButton,
       options: {
         datasets: [],
-        defaultDataset: {
-          name: {
+        defaultDataset: [
+          {
+            name: "name",
             label: "name",
             inputType: HTMLInputType.Text,
-            orderPosition: 1,
             options: {},
           },
-          label: {
+          {
+            name: "label",
             label: "label",
             inputType: HTMLInputType.Text,
-            orderPosition: 2,
             options: {},
           },
-          color: {
+          {
+            name: "color",
             label: "color",
             inputType: HTMLInputType.Color,
-            orderPosition: 3,
             options: {},
           },
-          xField: {
+          {
+            name: "xField",
             label: "x-axis field",
             inputType: HTMLInputType.Text,
-            orderPosition: 4,
             options: {},
           },
-          yField: {
+          {
+            name: "yField",
             label: "y-axis field",
             inputType: HTMLInputType.Text,
-            orderPosition: 5,
             options: {},
           },
-          chartStyle: {
+          {
+            name: "chartStyle",
             label: "chart style",
             inputType: HTMLInputType.Select,
-            orderPosition: 6,
             options: {
               options: ["dotted", "normal"],
               default: "normal",
             },
           },
-          areaUnderTheLineColor: {
+          {
+            name: "areaUnderTheLineColor",
             label: "color of area under line",
             inputType: HTMLInputType.Color,
-            orderPosition: 7,
             options: {},
           },
-        },
+        ],
       },
     },
-  },
+  ],
 };
 
 export const MixedChartType: VisualizationType = {
   name: "mixed-chart",
   icon: whiteMixedChartIcon,
-  config: {
-    barChartStyle: {
+  config: [
+    {
+      name: "barChartStyle",
       label: "bar chart style",
       inputType: HTMLInputType.Select,
-      orderPosition: 1,
       options: {
         options: ["stacked", "normal"],
         default: "normal",
       },
     },
-    orientation: {
+    {
+      name: "orientation",
       label: "orientation",
       inputType: HTMLInputType.Select,
-      orderPosition: 2,
       options: {
         options: ["horizontal", "vertical"],
         default: "horizontal",
       },
     },
-    addBarDataset: {
+    {
+      name: "addBarDataset",
       label: "add bar dataset",
-      inputType: HTMLInputType.Button,
-      orderPosition: 3,
+      inputType: HTMLInputType.AddMoreButton,
       options: {
         datasets: [],
-        defaultDataset: {
-          name: {
+        defaultDataset: [
+          {
+            name: "name",
             label: "name",
             inputType: HTMLInputType.Text,
-            orderPosition: 1,
             options: {},
           },
-          label: {
+          {
+            name: "label",
             label: "label",
             inputType: HTMLInputType.Text,
-            orderPosition: 2,
             options: {},
           },
-          color: {
+          {
+            name: "color",
             label: "color",
             inputType: HTMLInputType.Color,
-            orderPosition: 3,
             options: {},
           },
-          xField: {
+          {
+            name: "xField",
             label: "x-axis field",
             inputType: HTMLInputType.Text,
-            orderPosition: 4,
             options: {},
           },
-          yField: {
+          {
+            name: "yField",
             label: "y-axis field",
             inputType: HTMLInputType.Text,
-            orderPosition: 5,
             options: {},
           },
-        },
+        ],
       },
     },
-    addLineDataset: {
+    {
+      name: "addLineDataset",
       label: "add Line dataset",
-      inputType: HTMLInputType.Button,
-      orderPosition: 4,
+      inputType: HTMLInputType.AddMoreButton,
       options: {
         datasets: [],
-        defaultDataset: {
-          name: {
+        defaultDataset: [
+          {
+            name: "name",
             label: "name",
             inputType: HTMLInputType.Text,
-            orderPosition: 1,
             options: {},
           },
-          label: {
+          {
+            name: "label",
             label: "label",
             inputType: HTMLInputType.Text,
-            orderPosition: 2,
             options: {},
           },
-          color: {
+          {
+            name: "color",
             label: "color",
             inputType: HTMLInputType.Color,
-            orderPosition: 3,
             options: {},
           },
-          xField: {
+          {
+            name: "xField",
             label: "x-axis field",
             inputType: HTMLInputType.Text,
-            orderPosition: 4,
             options: {},
           },
-          yField: {
+          {
+            name: "yField",
             label: "y-axis field",
             inputType: HTMLInputType.Text,
-            orderPosition: 5,
             options: {},
           },
-          chartStyle: {
+          {
+            name: "chartStyle",
             label: "chart style",
             inputType: HTMLInputType.Select,
-            orderPosition: 6,
             options: {
               options: ["dotted", "normal"],
               default: "normal",
             },
           },
-          areaUnderTheLineColor: {
+          {
+            name: "areaUnderTheLineColor",
             label: "color of area under line",
             inputType: HTMLInputType.Color,
-            orderPosition: 7,
             options: {},
           },
-        },
+        ],
       },
     },
-  },
+  ],
 };
 
 export const PieChartType: VisualizationType = {
   name: "pie-chart",
   icon: whitePieChartIcon,
-  config: {
-    labelField: {
+  config: [
+    {
+      name: "labelField",
       label: "label field",
       inputType: HTMLInputType.Text,
-      orderPosition: 1,
       options: {},
     },
-    valueField: {
+    {
+      name: "valueField",
       label: "value field",
       inputType: HTMLInputType.Text,
-      orderPosition: 2,
       options: {},
     },
-  },
+  ],
 };
 
 export const DonutChartType: VisualizationType = {
   name: "donut-chart",
   icon: whiteDonutChartIcon,
-  config: {
-    labelField: {
+  config: [
+    {
+      name: "labelField",
       label: "label field",
       inputType: HTMLInputType.Text,
-      orderPosition: 1,
       options: {},
     },
-    valueField: {
+    {
+      name: "valueField",
       label: "value field",
       inputType: HTMLInputType.Text,
-      orderPosition: 2,
       options: {},
     },
-  },
+  ],
 };
 
 export const UnorderedListType: VisualizationType = {
   name: "unordered-list",
   icon: whiteListIcon,
-  config: {
-    valueField: {
+  config: [
+    {
+      name: "valueField",
       label: "value field",
       inputType: HTMLInputType.Text,
-      orderPosition: 1,
       options: {},
     },
-    style: {
+    {
+      name: "style",
       label: "style",
       inputType: HTMLInputType.Select,
-      orderPosition: 2,
       options: {
         options: ["circle", "square"],
       },
     },
-  },
+  ],
 };
 
 export const OrderedListType: VisualizationType = {
   name: "ordered-list",
   icon: whiteNumberdListIcon,
-  config: {
-    valueField: {
+  config: [
+    {
+      name: "valueField",
       label: "value field",
       inputType: HTMLInputType.Text,
-      orderPosition: 1,
       options: {},
     },
-    style: {
+    {
+      name: "style",
       label: "style",
       inputType: HTMLInputType.Select,
-      orderPosition: 2,
       options: {
         options: ["upper-roman", "lower-roman", "upper-alpha", "lower-alpha"],
       },
     },
-  },
+  ],
 };
 
 export const TextType: VisualizationType = {
   name: "text",
   icon: whiteTextIcon,
-  config: {
-    valueField: {
+  config: [
+    {
+      name: "valueField",
       label: "value field",
       inputType: HTMLInputType.Text,
-      orderPosition: 1,
       options: {},
     },
-    fontSize: {
+    {
+      name: "fontSize",
       label: "font size",
       inputType: HTMLInputType.Range,
-      orderPosition: 2,
       options: {
         min: 8,
         max: 600,
         default: 16,
       },
     },
-    bold: {
+    {
+      name: "bold",
       label: "bold",
       inputType: HTMLInputType.Checkbox,
-      orderPosition: 3,
       options: {
         default: false,
       },
     },
-    italic: {
+    {
+      name: "italic",
       label: "italic",
       inputType: HTMLInputType.Checkbox,
-      orderPosition: 4,
       options: {
         default: false,
       },
     },
-    alignment: {
+    {
+      name: "alignment",
       label: "alignment",
       inputType: HTMLInputType.Select,
-      orderPosition: 5,
       options: {
         default: "left",
         options: ["left", "center", "right"],
       },
     },
-  },
+  ],
 };
+
+export const visualizationTypeMap: { [key: string]: VisualizationType } = {
+  [TableType.name]: TableType,
+  [ScatterChartType.name]: ScatterChartType,
+  [BarChartType.name]: BarChartType,
+  [MultipleBarChartType.name]: MultipleBarChartType,
+  [LineChartType.name]: LineChartType,
+  [MultipleLineChartType.name]: MultipleLineChartType,
+  [MixedChartType.name]: MixedChartType,
+  [PieChartType.name]: PieChartType,
+  [DonutChartType.name]: DonutChartType,
+  [TextType.name]: TextType,
+  [OrderedListType.name]: OrderedListType,
+  [UnorderedListType.name]: UnorderedListType,
+};
+
+export const visualizationTypeList = Object.keys(visualizationTypeMap).sort();
